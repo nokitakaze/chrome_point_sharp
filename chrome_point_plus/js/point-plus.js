4a8d6f49f4961e03d2eb5343c2ccc197b1b82199
isqua <isqua@isqua.ru>
2015-01-08 18:53:24 +0300

    Move OptionsManager to separate file

--- chrome_point_plus/js/point-plus.js
+++ chrome_point_plus/js/point-plus.js
@@ -5,45 +5,6 @@ chrome.runtime.sendMessage({
     console.debug('showPageAction response: %O', response);
 });
 
-// @todo Move OptionsManager to the separate file
-/**
- * Объект для получения опций
- * @param {Object} options Хеш настроек
- * @constructor
- */
-function OptionsManager(options) {
-    this._options = options || {};
-}
-
-/**
- * @param {String} optionName Имя опции
- * @returns {Boolean|String|Null} Значение опции
- */
-OptionsManager.prototype.get = function(optionName) {
-    return this._options.hasOwnProperty(optionName) ? this._options[optionName].value : null;
-};
-
-/**
- * Проверяет, равна ли опция значению value. Если value не переданно, проверяет задана ли она и не равна ли false/''
- * @param {String} optionName Имя опции
- * @param {Boolean|String} [value=true] Значение опции
- * @returns {Boolean}
- */
-OptionsManager.prototype.is = function(optionName, value) {
-    if (typeof value !== 'undefined') {
-        return this.get(optionName) === value;
-    } else {
-        return Boolean(this.get(optionName));
-    }
-};
-
-/**
- * @returns {Object} Хеш опций
- */
-OptionsManager.prototype.getOptions = function() {
-    return this._options;
-};
-
 var ppVersion;
 
 chrome.runtime.sendMessage(null, {
