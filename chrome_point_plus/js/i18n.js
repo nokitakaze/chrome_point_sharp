1020be808eb3eabd46591b6388192f522427d474
Nokita Kaze <admin@kanaria.ru>
2015-01-16 07:46:00 +0300

    * Враппер над storage для Fx * Часть кода для Options * Иконка сделана * Почти сделаны иконки * Картинка в баре есть * Почти победил urlbar-icons * Cross origin готов

--- chrome_point_plus/js/i18n.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Processing all emenents contains data-i18n attribute
-    Array.prototype.forEach.call(document.querySelectorAll('[data-i18n]'), function(elem) {
-        elem.innerHTML = chrome.i18n.getMessage(elem.dataset.i18n);
-    });
-}, false);
---
+++ resources/point_sharp/data/js/point_test.js
@@ -0,0 +1,5 @@
+// self.port.emit('showurlicon', null);
+console.info("point test.js");
+
+console.info("point_sharp_options_tree: %O", point_sharp_options_tree);
+
