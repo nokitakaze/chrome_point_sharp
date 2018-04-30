c024505da8d2136d0c72740da824cfaed34bcced
Nokita Kaze <admin@kanaria.ru>
2018-04-30 08:00:07 +0300

    * Перемещаем css-additional * По просьбе RainbowSpike добавляем padding для редактора маркдауна

--- build/build.php
+++ build/build.php
@@ -15,18 +15,18 @@
      */
     $build_version = json_decode(file_get_contents($root_folder.'/build/build_version.json'));
     $build_version->version++;
-    file_put_contents($root_folder.'/build/build_version.json', json_encode(array(
+    file_put_contents($root_folder.'/build/build_version.json', json_encode([
         'last_build_time' => time(),
         'last_build_time_str' => gmdate('Y-m-d H:i:sO'),
-        'version' => $build_version->version
-    )));
+        'version' => $build_version->version,
+    ]));
 
     // Меняем контент
     foreach (
-        array(
-            array('other/bower.json', 'bower.json'),
-            array('other/manifest.json', 'chrome_point_plus/manifest.json'),
-        ) as $pair) {
+        [
+            ['other/bower.json', 'bower.json'],
+            ['other/manifest.json', 'chrome_point_plus/manifest.json'],
+        ] as $pair) {
         // Берём контент
         $content = file_get_contents($root_folder.'/build/'.$pair[0]);
 
@@ -45,10 +45,9 @@
         return str_replace(['\'', '"'], ['\\\'', '\\"'], $s);
     }
 
+    /*
     // Копируем Папки
     foreach (array(
-                 array('vendor', 'chrome_point_plus/vendor'),
-                 array('css', 'chrome_point_plus/css/additional'),
              ) as $pair) {
         if (file_exists($root_folder.'/'.$pair[1])) {
             system('rd "'.addslashes_quote($root_folder.'\\'.str_replace('/', '\\', $pair[1])).'" /S /Q');
@@ -57,6 +56,7 @@
         system('xcopy "'.addslashes_quote($root_folder.'\\build\\'.str_replace('/', '\\', $pair[0])).'" "'.
                addslashes_quote($root_folder.'\\'.str_replace('/', '\\', $pair[1])).'\\" /E /Y');
     }
+    */
 
     echo "Version ".$json->version.'.'.$build_version->version.' builded at '.gmdate('Y-m-d H:i:sO')."\n";
 ?>
